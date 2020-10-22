71186dd4c25eb47bdc0d3ad65ea38696e0156cf2
Vishant Singh <vishant_singh@outlook.com>
2020-10-22 12:06:17 +0100

    Add windows setup

---
+++ windows/animalApp/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::animalApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::animalApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
